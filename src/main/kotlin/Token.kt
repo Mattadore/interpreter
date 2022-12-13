658baf67f5dadcd7433418773e13c030e77b8161
Matt Buckley <me@matt.software>
2022-12-12 19:03:18 -0800

    remove files?

--- src/main/kotlin/Token.kt
+++
@@ -1,7 +0,0 @@
-package com.craftinginterpreters.lox
-
-internal class Token(private val type: TokenType, private val lexeme: String, private val literal: Any?, private val line: Int) {
-    override fun toString(): String {
-        return "$type $lexeme $literal"
-    }
-}
